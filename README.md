91db9c235880313864582c4690cec20dd7405701
Thomas Steven <thomaspatricksteven@gmail.com>
2021-12-01 21:53:43 -0500

    update

---
+++ README.md
@@ -0,0 +1,33 @@
+# Zlak
+
+A scriptable fake chat application
+
+![zlak](https://i.imgur.com/QYoQ2yy.png)
+
+## Description
+
+If you need to look like you are in a chat, this is the application for you.
+
+## How does it work?
+
+1. Write a script file.
+2. Upload the script file to Zlak [zlak.tomontheinternet.com](https://zlak.tomontheinternet.com) by clicking on the header.
+3. Enjoy
+
+## Script file
+
+A script file is a set of instructions to Zlak. For an example, look at `sample-instructions` in this repo.
+
+The available commands are:
+
+```
+USER `username`          // set the name of the user, defaults to Tom if not called
+WAIT 2000                // waits for the number of milliseconds, in this case 2000 (2 seconds)
+WAIT USER                // waits until the user sends a message
+TYPING `username` 2000   // show a typing message, in this case "username is typing..." for 2 seconds
+NARRATE `message` 5000   // show a narration message, in this case it show "narrate" for 5 seconds
+```
+
+## Why did you build this?
+
+I thought it might be fun to have someone to chat to when recording YouTube videos.
--- app.js
+++ app.js
@@ -1,20 +1,77 @@
-console.log("hey");
-
+const $header = document.querySelector("header");
 const $messages = document.getElementById("messages");
+const $form = document.querySelector("form");
+const $input = document.querySelector("#message-input");
+const $fileUpload = document.querySelector("#file-upload");
+
+/** userAction knows if the user performed an action since last check */
+let userAction = false;
+
+let username = "Tom"; // Default user is Tom
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  sendMessage(username, $input.value.trim());
+
+  $form.reset();
+
+  userAction = true;
+}
+
+function onClickHeader(e) {
+  $fileUpload.click();
+}
+
+async function onFileUpload() {
+  reset();
+
+  if (!this.files.length) {
+    return;
+  }
+
+  const file = this.files[0];
+
+  let instructions = [];
+  try {
+    instructions = await getInstructions(file);
+  } catch (error) {
+    alert("invalid instructions");
+    console.log(error);
+
+    return;
+  }
+
+  followInstructions(instructions);
+}
+
+function reset() {
+  userAction = false;
+  $messages.innerHTML = "";
+}
+
+async function followInstructions(instructions) {
+  console.log("Running instructions.");
+  for (const instruction of instructions) {
+    const line = await instruction();
+    console.log("Line complete:", line);
+  }
+  console.log("Instructions complete.");
+}
 
 /**
  * sendMessage adds a text message to the dom
  */
 function sendMessage(author, text) {
-  var $author = document.createElement("div");
+  const $author = document.createElement("div");
   $author.classList.add("author");
   $author.innerHTML = author;
 
-  var $text = document.createElement("div");
+  const $text = document.createElement("div");
   $text.classList.add("text");
   $text.innerHTML = text;
 
-  var $message = document.createElement("div");
+  const $message = document.createElement("div");
   $message.classList.add("message");
 
   $message.appendChild($author);
@@ -22,8 +79,187 @@ function sendMessage(author, text) {
   $messages.appendChild($message);
 }
 
-sendMessage(
-  "Bill",
-  "What the fuck? You are everything in the world to me and I hope that's obvious. What the fuck? You are everything in the world to me and I hope that's obvious."
-);
-sendMessage("Bill", "What the fuck?");
+function narrate(message, waitTime) {
+  const $narration = document.createElement("div");
+  $narration.classList.add("narration");
+  $narration.innerHTML = message;
+
+  $messages.appendChild($narration);
+  setTimeout(() => {
+    $narration.remove();
+  }, waitTime);
+}
+
+/**
+ * typingIndicator show that someone is typing
+ */
+function typingIndicator(author, time) {
+  const $typingIndicator = document.createElement("div");
+  $typingIndicator.classList.add("typing-indicator");
+  $typingIndicator.innerHTML = `${author} is typing...`;
+
+  $messages.appendChild($typingIndicator);
+  setTimeout(() => {
+    $typingIndicator.remove();
+  }, time);
+}
+
+function getInstructions(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      try {
+        const file = e.target.result;
+        const instructions = file
+          .split(/\r\n|\n/)
+          .filter((l) => l)
+          .map((l) => l.trim())
+          .map((l) => makeInstruction(l));
+        resolve(instructions);
+      } catch (error) {
+        reject(error);
+      }
+    };
+
+    reader.readAsText(file);
+  });
+}
+
+function makeInstruction(line) {
+  if (line.startsWith("USER")) {
+    return makeUserInstruction(line);
+  }
+
+  if (line.startsWith("WAIT") && !line.includes("USER")) {
+    return makeWaitTimeInstruction(line);
+  }
+
+  if (line.startsWith("WAIT") && line.includes("USER")) {
+    return makeWaitUserInstruction(line);
+  }
+
+  if (line.startsWith("SAY")) {
+    return makeSayInstruction(line);
+  }
+
+  if (line.startsWith("TYPING")) {
+    return makeTypingInstruction(line);
+  }
+
+  if (line.startsWith("NARRATE")) {
+    return makeNarrationInstruction(line);
+  }
+
+  alert("invalid instruction");
+  console.log("invalid instruction", line);
+}
+
+function makeWaitUserInstruction(line) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        if (userAction) {
+          clearInterval(interval);
+          userAction = false;
+          resolve(line);
+        }
+      }, 10);
+    });
+  };
+}
+
+function makeWaitTimeInstruction(line) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      const waitTime = line.split(" ")[1].trim();
+      setTimeout(() => {
+        resolve(line);
+      }, waitTime);
+    });
+  };
+}
+
+function makeSayInstruction(line) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      const [author, message] = extractMessage(line);
+      sendMessage(author, message);
+      resolve(line);
+    });
+  };
+}
+
+function makeUserInstruction(line) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      username = line.match(/(?:`[^`]*`|^[^`]*$)/g)[0].replace(/`/g, "");
+      resolve(line);
+    });
+  };
+}
+
+function makeNarrationInstruction(line) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      const [narration, waitTime] = extractTyping(line);
+      narrate(narration, waitTime);
+      setTimeout(() => {
+        resolve(line);
+      }, waitTime);
+    });
+  };
+}
+
+function makeTypingInstruction(line) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      const [author, waitTime] = extractTyping(line);
+      typingIndicator(author, waitTime);
+      setTimeout(() => {
+        resolve(line);
+      }, waitTime);
+    });
+  };
+}
+
+function extractMessage(line) {
+  return line.match(/(?:`[^`]*`|^[^`]*$)/g).map((x) => x.replace(/`/g, ""));
+}
+
+function extractTyping(line) {
+  const author = line.match(/(?:`[^`]*`|^[^`]*$)/g)[0].replace(/`/g, "");
+  const time = line.split(" ").reverse()[0];
+
+  return [author, time];
+}
+
+function extractNarration(line) {
+  return extractTyping(line);
+}
+
+async function wait(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+}
+
+// demo runs a demo on app launch
+// the code is rough, but works.
+async function demo() {
+  typingIndicator("Bot", 2000);
+  await wait(2000);
+  sendMessage(
+    "Bot",
+    "Hey! Welcome to Zlack. To find out more, go to https://github.com/tom-on-the-internet/zlak"
+  );
+}
+
+$form.addEventListener("submit", onSubmit);
+$header.addEventListener("click", onClickHeader);
+
+$fileUpload.addEventListener("change", onFileUpload);
+
+demo();
